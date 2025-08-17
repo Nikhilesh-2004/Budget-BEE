487af71da26c2e88bb317c1a8ce2c6d9f3bf5726
Nikhilesh-2004 <158011724+Nikhilesh-2004@users.noreply.github.com>
2025-08-17 22:13:35 +0530

    Add files via upload

---
+++ backend/router/expensedetailsrouter.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const {
+  postExpenseDetails,
+  getExpenses,
+  deleteExpense,
+  editExpense,
+  deleteAllExpenses,
+} = require("../controllers.js/expensedetailscontroller");
+
+const postExpensesRouter = express.Router();
+postExpensesRouter.post("/userexpense", postExpenseDetails);
+postExpensesRouter.get("/userexpenses", getExpenses);
+postExpensesRouter.delete("/deleteexpense/:id", deleteExpense);
+postExpensesRouter.delete("/userexpenses", deleteAllExpenses);
+postExpensesRouter.put("/editexpense/:id", editExpense); // 👈 New route added
+
+
+module.exports = postExpensesRouter;
